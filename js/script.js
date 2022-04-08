cc11429d8109ac110fc068325efaae03976a4000
Tu Nguyen <tuinfor@ymail.com>
2022-04-08 03:02:47 -0400

    try to fitugure out how to get data from the APIs

--- js/script.js
+++ js/script.js
@@ -1,24 +1,41 @@
-var cityEl = document.querySelector('#city');
+var citySeachFormEl = document.querySelector('#city-search-form');
+var cityEl = document.querySelector('#city')
+var displayResultEl = document.querySelector('#display-results')
+var resultEl = document.querySelector('#result')
+//hander search form 
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  var citySearch = cityEl.value.trim(); //trim to clean the space on search text
+  if (citySearch) {
+    getCityRepos(citySearch);
+    displayResultEl.textContent ='';
+    cityEl.value = '';
+  } else {
+    cityEl.value = 'Please enter a city'
+  }
+}
 
-var api = 'a5fc4ee8330414cf46eb731642cac3df'
-var city = 'Atlanta';
-var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + api;
-
-  fetch(apiUrl)
+function getCityRepos () {
+  var apiKeys = 'a5fc4ee8330414cf46eb731642cac3df'
+  var city = cityEl.value
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKeys}`)
   .then(function (response) {
-  return response.json(); 
+    if (response.ok) {
+      console.log('status working...') 
+      return response.json();
+    }else {
+      console.log('status not working...');
+    }
   })
-  .then(function (data) {
-    console.log(data);
-  })
-
-// fetch('https://api.github.com/orgs/twitter/repos')
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log('Twitter Repositories: Names only \n----------');
-//     for (var i = 0; i < data.length; i++) {
-//       console.log(data[i].name);
-//     }
-//   });
+  .then (function (data) {
+    if (data ==  undefined) {
+      console.log('Please enter the city') 
+    } else {
+      console.log(data)
+      
+    }
+             
+    })
+}
+getCityRepos ()
+citySeachFormEl.addEventListener('submit', formSubmitHandler); 
